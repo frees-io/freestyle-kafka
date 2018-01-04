ce3e4343089e5db3dd5ce201cb114339bbeab071
AdrianRaFo <adrianramirezfornell@gmail.com>
2018-01-04 18:51:35 +0100

    Update to freestyle 0.5.1 and cats 1.0.1

--- core/src/main/scala/producer/KafkaProducer.scala
+++ core/src/main/scala/producer/KafkaProducer.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 47 Degrees, LLC. <http://www.47deg.com>
+ * Copyright 2017-2018 47 Degrees, LLC. <http://www.47deg.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,7 +22,9 @@ import java.util.concurrent.atomic.{AtomicBoolean, AtomicReference}
 
 import cats.MonadError
 import cats.implicits._
+import freestyle.free._
 import freestyle.async._
+import freestyle.free.async._
 import org.apache.kafka.clients.consumer.OffsetAndMetadata
 import org.apache.kafka.clients.producer._
 import org.apache.kafka.common.{Metric, MetricName, PartitionInfo, TopicPartition}
